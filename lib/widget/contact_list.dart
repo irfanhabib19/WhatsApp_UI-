c2cb14597ff8583b7d5a2caf920ff5d5ecdb64b1
habibirfan180 <habibirfan180@gmail.com>
2024-12-02 23:47:06 +0530

    commit T

--- lib/widget/contact_list.dart
+++ lib/widget/contact_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/colors.dart';
 import 'package:whatsapp_ui/info.dart';
 
 class ContactList extends StatelessWidget {
@@ -8,32 +9,46 @@ class ContactList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(
-              info[index]['name'].toString(),
-              style: TextStyle(fontSize: 18),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: ListTile(
+                    title: Text(
+                      info[index]['name'].toString(),
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 6),
+                      child: Text(
+                        info[index]['message'].toString(),
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ),
+                    leading: CircleAvatar(
+                      radius: 22,
+                      backgroundImage: NetworkImage(
+                        info[index]['profilePic'].toString(),
+                      ),
+                    ),
+                    trailing: Text(
+                      info[index]["time"].toString(),
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                );
+              },
+              itemCount: info.length,
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 6),
-              child: Text(
-                info[index]['message'].toString(),
-                style: TextStyle(fontSize: 14),
-              ),
-            ),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                info[index]['profilePic'].toString(),
-              ),
-            ),
-            trailing: Text(
-              info[index]["time"].toString(),
-              style: TextStyle(fontSize: 12),
-            ),
-          );
-        },
-        itemCount: info.length,
+          ),
+          Divider(
+            color: dividerColor,
+            indent: 85,
+          )
+        ],
       ),
     );
   }
