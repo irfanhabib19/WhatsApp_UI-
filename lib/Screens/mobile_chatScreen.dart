18f091f6c0ca0f28b49d643f1353c780b04df328
habibirfan180 <habibirfan180@gmail.com>
2024-12-07 23:57:09 +0530

    complete

--- lib/Screens/mobile_chatScreen.dart
+++ lib/Screens/mobile_chatScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui/colors.dart';
 import 'package:whatsapp_ui/info.dart';
+import 'package:whatsapp_ui/widget/chat_list.dart';
 
 class MobileChatscreen extends StatelessWidget {
   const MobileChatscreen({super.key});
@@ -15,6 +16,50 @@ class MobileChatscreen extends StatelessWidget {
           IconButton(onPressed: () {}, icon: Icon(Icons.video_call_outlined)),
           IconButton(onPressed: () {}, icon: Icon(Icons.call)),
           IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(child: ChatList()),
+          TextField(
+            decoration: InputDecoration(
+              fillColor: mobileChatBoxColor,
+              filled: true,
+              prefixIcon: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Icon(
+                  Icons.emoji_emotions,
+                  color: Colors.grey,
+                ),
+              ),
+              suffixIcon: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(
+                      Icons.camera_alt,
+                      color: Colors.grey,
+                    ),
+                    Icon(
+                      Icons.attach_file,
+                      color: Colors.grey,
+                    ),
+                    Icon(
+                      Icons.attach_money,
+                      color: Colors.grey,
+                    )
+                  ],
+                ),
+              ),
+              hintText: "Type a message",
+              hintStyle: TextStyle(fontSize: 15, color: Colors.white),
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(21),
+              ),
+            ),
+          )
         ],
       ),
     );
